9d16558a4877d70bf3ff48e9e53a1faee6b070df
Tyree Byrd <gfan23@hotmail.com>
2021-12-15 11:06:58 -0800

    adding more info on read

--- user-imput/ui-example.sh
+++ user-imput/ui-example.sh
@@ -1,4 +1,4 @@
-# 'read' command request user to input text
 echo "what is your name?"
 read name
 echo "Hello $name."
+# 'read' command request user to input text, we use $ to recall the variable established by 'read'
